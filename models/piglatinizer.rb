338e6c44c9f4eb5da27e1c27bcdef9e2a476f572
Andrew Nikonchuk <anikonch@gmail.com>
2018-08-13 19:03:21 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,8 +1,8 @@
 require 'pry'
 class PigLatinizer
 
-  def vowel?(letter)
-    ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"].include?(letter)
+  def consonant?(letter)
+    ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z", "B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "X", "Y", "Z"].include?(letter)
   end
 
   def piglatinize_word(word)
